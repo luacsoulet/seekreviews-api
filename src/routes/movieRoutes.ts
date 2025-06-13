487de5e9954580e6359e6c557daae897ccd9a0c2
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-06-13 14:11:50 +0200

    Import get movie by ID routes

--- src/routes/movieRoutes.ts
+++ src/routes/movieRoutes.ts
@@ -1,9 +1,13 @@
 import { FastifyInstance } from "fastify";
-import { getMovies } from "../controllers/movieControllers";
-import { getMoviesSchema } from "../dtos/movieDtos";
+import { getMovieById, getMovies } from "../controllers/movieControllers";
+import { getMovieByIdSchema, getMoviesSchema } from "../dtos/movieDtos";
 
 export const movieRoutes = async (fastify: FastifyInstance) => {
     fastify.get('/', {
         schema: getMoviesSchema,
     }, getMovies);
+
+    fastify.get('/:id', {
+        schema: getMovieByIdSchema,
+    }, getMovieById);
 }
