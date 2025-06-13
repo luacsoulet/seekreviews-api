98cb4cba999d9b50f480748327fd272e0f7940db
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-06-14 01:59:48 +0200

    Import delete movie routes

--- src/routes/movieRoutes.ts
+++ src/routes/movieRoutes.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
-import { createMovie, getMovieByGenre, getMovieById, getMovieByTitle, getMovies, modifyMovie } from "../controllers/movieControllers";
-import { createMovieSchema, getMovieByGenreSchema, getMovieByIdSchema, getMovieByTitleSchema, getMoviesSchema, modifyMovieSchema } from "../dtos/movieDtos";
+import { createMovie, deleteMovie, getMovieByGenre, getMovieById, getMovieByTitle, getMovies, modifyMovie } from "../controllers/movieControllers";
+import { createMovieSchema, deleteMovieSchema, getMovieByGenreSchema, getMovieByIdSchema, getMovieByTitleSchema, getMoviesSchema, modifyMovieSchema } from "../dtos/movieDtos";
 import { authenticate } from "../middleware/auth";
 
 export const movieRoutes = async (fastify: FastifyInstance) => {
@@ -29,4 +29,9 @@ export const movieRoutes = async (fastify: FastifyInstance) => {
         schema: modifyMovieSchema,
         preHandler: [authenticate]
     }, modifyMovie);
+
+    fastify.delete('/:id', {
+        schema: deleteMovieSchema,
+        preHandler: [authenticate]
+    }, deleteMovie);
 }
