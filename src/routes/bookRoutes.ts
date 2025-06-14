8a438e6edeecbed1f02ddd3b9a7ab6c23e1df489
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-06-14 02:42:33 +0200

    Import controllers to modify a book for admin-only

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
-import { getBookById, getBookByGenre, getBookByTitle, getBooks, createBook } from "../controllers/bookControllers";
-import { getBookByIdSchema, getBookByGenreSchema, getBookByTitleSchema, getBooksSchema, createBookSchema } from "../dtos/bookDtos";
+import { getBookById, getBookByGenre, getBookByTitle, getBooks, createBook, modifyBook } from "../controllers/bookControllers";
+import { getBookByIdSchema, getBookByGenreSchema, getBookByTitleSchema, getBooksSchema, createBookSchema, modifyBookSchema } from "../dtos/bookDtos";
 import { authenticate } from "../middleware/auth";
 
 export const bookRoutes = async (fastify: FastifyInstance) => {
@@ -24,4 +24,9 @@ export const bookRoutes = async (fastify: FastifyInstance) => {
         schema: createBookSchema,
         preHandler: [authenticate]
     }, createBook);
+
+    fastify.patch('/:id', {
+        schema: modifyBookSchema,
+        preHandler: [authenticate]
+    }, modifyBook);
 }
